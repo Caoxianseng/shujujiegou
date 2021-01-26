727d907c5f715bcab05c00ecf3f5df684a09b755
曹先森 <1669928567@qq.com>
2021-01-26 22:39:01 +0800

    0124简单熟悉框架

---
+++ lianbiao_0124/Test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+#include<string.h>
+
+typedef struct Node {
+	int data;//数据域
+	PNODE pNext;//这就是指向下一个结点的指针
+}*PNODE,NODE;
+int main() {
+
+
+	return 0;
+}
